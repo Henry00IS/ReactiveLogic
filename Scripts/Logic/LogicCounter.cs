e9ed73eafd0e7c4aceae217c8f754912be14b743
Henry de Jongh <henry00@live.nl>
2023-03-08 17:02:45 +0100

    Added Initial Value, Clamp Min, Clamp Max, Set Min (+Invoke), Set Max (+Invoke), Freeze and Unfreeze to Logic Counter.

--- Scripts/Logic/LogicCounter.cs
+++ Scripts/Logic/LogicCounter.cs
@@ -24,21 +24,73 @@ namespace AlpacaIT.ReactiveLogic
             new MetaInterface(MetaInterfaceType.Input, "Add", "Adds the float parameter to the counter without invoking the output.", "amount", MetaParameterType.Float, "The number to be added to the counter."),
             new MetaInterface(MetaInterfaceType.Input, "AddInvoke", "Adds the float parameter to the counter and invokes the output with the result.", "amount", MetaParameterType.Float, "The number to be added to the counter."),
             new MetaInterface(MetaInterfaceType.Input, "Multiply", "Multiplies the counter by the float parameter without invoking the output.", "amount", MetaParameterType.Float, "The number to multiply the counter with."),
-            new MetaInterface(MetaInterfaceType.Input, "MultiplyInvoke", "Multiplies the counter by the float parameter and invokes the output.", "amount", MetaParameterType.Float, "The number to multiply the counter with."),
+            new MetaInterface(MetaInterfaceType.Input, "MultiplyInvoke", "Multiplies the counter by the float parameter and invokes the output with the result.", "amount", MetaParameterType.Float, "The number to multiply the counter with."),
             new MetaInterface(MetaInterfaceType.Input, "Divide", "Divides the counter by the float parameter without invoking the output. If this causes a division by zero then the counter value will be unchanged.", "amount", MetaParameterType.Float, "The number to divide the counter with."),
-            new MetaInterface(MetaInterfaceType.Input, "DivideInvoke", "Divides the counter by the float parameter and invokes the output. If this causes a division by zero then the counter value will be unchanged.", "amount", MetaParameterType.Float, "The number to divide the counter with."),
+            new MetaInterface(MetaInterfaceType.Input, "DivideInvoke", "Divides the counter by the float parameter and invokes the output with the result. If this causes a division by zero then the counter value will be unchanged.", "amount", MetaParameterType.Float, "The number to divide the counter with."),
             new MetaInterface(MetaInterfaceType.Input, "Subtract", "Subtracts the float parameter from the counter without invoking the output.", "amount", MetaParameterType.Float, "The number to be subtracted from the counter."),
             new MetaInterface(MetaInterfaceType.Input, "SubtractInvoke", "Subtracts the float parameter from the counter and invokes the output with the result.", "amount", MetaParameterType.Float, "The number to be subtracted from the counter."),
             new MetaInterface(MetaInterfaceType.Input, "SetValue", "Sets the counter to the float parameter without invoking the output.", "amount", MetaParameterType.Float, "The number the counter will be set to."),
             new MetaInterface(MetaInterfaceType.Input, "SetValueInvoke", "Sets the counter to the float parameter and invokes the output with the result.", "amount", MetaParameterType.Float, "The number the counter will be set to."),
+
+            new MetaInterface(MetaInterfaceType.Input, "SetMin", "Sets the minimum value of the counter to the float parameter without invoking the output.", "amount", MetaParameterType.Float, "The minimum number the counter can be set to. If both min and max are set to zero, no clamping will occur."),
+            new MetaInterface(MetaInterfaceType.Input, "SetMinInvoke", "Sets the minimum value of the counter to the float parameter and invokes the output with the result.", "amount", MetaParameterType.Float, "The minimum number the counter can be set to. If both min and max are set to zero, no clamping will occur."),
+            new MetaInterface(MetaInterfaceType.Input, "SetMax", "Sets the maximum value of the counter to the float parameter without invoking the output.", "amount", MetaParameterType.Float, "The maximum number the counter can be set to. If both min and max are set to zero, no clamping will occur."),
+            new MetaInterface(MetaInterfaceType.Input, "SetMaxInvoke", "Sets the maximum value of the counter to the float parameter and invokes the output with the result.", "amount", MetaParameterType.Float, "The maximum number the counter can be set to. If both min and max are set to zero, no clamping will occur."),
+
+            new MetaInterface(MetaInterfaceType.Input, "Freeze", "Freezes the counter value so that it cannot be changed by inputs. This makes the counter read-only, which is very useful for the SetMin and SetMax operations, which would otherwise always forcibly clamp the value before you could set the other field."),
+            new MetaInterface(MetaInterfaceType.Input, "Unfreeze", "Unfreezes the counter value so that it can be changed by inputs."),
             new MetaInterface(MetaInterfaceType.Input, "Invoke", "Invokes the output with the current value of the counter."),
 
             new MetaInterface(MetaInterfaceType.Output, "Invoked", "Invoked when an operation on the current value of the counter finished.", "amount", MetaParameterType.Float, "The current value of the counter.")
         );
 
-        /// <summary>The current value of the counter.</summary>
-        [Tooltip("The current value of the counter.")]
-        public float currentValue = 0.0f;
+        /// <summary>
+        /// The initial and current value of the counter. Use <see cref="SetCurrentValue"/> to set
+        /// this field and respect the counter being frozen or clamped by the user.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("The initial and current value of the counter.")]
+        private float currentValue = 0f;
+
+        /// <summary>
+        /// This is the minimum value of the counter. It cannot go below this value. If both <see
+        /// cref="clampMin"/> and <see cref="clampMax"/> are set to zero, no clamping will occur.
+        /// </summary>
+        [Tooltip("This is the minimum value of the counter. It cannot go below this value. If both Clamp Min and Clamp Max are set to zero, no clamping will occur.")]
+        public float clampMin = 0f;
+
+        /// <summary>
+        /// This is the maximum value of the counter. It cannot go above this value. If both <see
+        /// cref="clampMin"/> and <see cref="clampMax"/> are set to zero, no clamping will occur.
+        /// </summary>
+        [Tooltip("This is the maximum value of the counter. It cannot go above this value. If both Clamp Min and Clamp Max are set to zero, no clamping will occur.")]
+        public float clampMax = 0f;
+
+        /// <summary>
+        /// Whether the counter value is frozen so that it cannot be changed by inputs.
+        /// </summary>
+        [Tooltip("Whether the counter value is frozen so that it cannot be changed by inputs.")]
+        public bool frozen = false;
+
+        /// <summary>
+        /// Sets and processes the <see cref="currentValue"/> to ensure it meets all criteria of the
+        /// counter such as clamping or freezing the value.
+        /// </summary>
+        /// <param name="number">The number the counter will maybe be set to.</param>
+        public void SetCurrentValue(float number)
+        {
+            // if the counter value has been frozen we do nothing.
+            if (frozen) return;
+
+            // set the current value to the number.
+            currentValue = number;
+
+            // we now perform clamping on the current value.
+            if (clampMin != 0f || clampMax != 0f)
+            {
+                currentValue = Mathf.Clamp(currentValue, clampMin, clampMax);
+            }
+        }
 
         public void OnReactiveInput(ReactiveInput input)
         {
@@ -46,17 +98,25 @@ namespace AlpacaIT.ReactiveLogic
 
             switch (input.name)
             {
-                case "Add": currentValue += inputValue; break;
-                case "AddInvoke": currentValue += inputValue; InvokeOutput(input); break;
-                case "Multiply": currentValue *= inputValue; break;
-                case "MultiplyInvoke": currentValue *= inputValue; InvokeOutput(input); break;
-                case "Divide": if (inputValue != 0f) currentValue /= inputValue; break;
-                case "DivideInvoke": if (inputValue != 0f) currentValue /= inputValue; InvokeOutput(input); break;
-                case "Subtract": currentValue -= inputValue; break;
-                case "SubtractInvoke": currentValue -= inputValue; InvokeOutput(input); break;
-                case "SetValue": currentValue = inputValue; break;
-                case "SetValueInvoke": currentValue = inputValue; InvokeOutput(input); break;
-                case "Invoke": InvokeOutput(input); break;
+                case "Add": SetCurrentValue(currentValue + inputValue); break;
+                case "AddInvoke": SetCurrentValue(currentValue + inputValue); InvokeOutput(input); break;
+                case "Multiply": SetCurrentValue(currentValue * inputValue); break;
+                case "MultiplyInvoke": SetCurrentValue(currentValue * inputValue); InvokeOutput(input); break;
+                case "Divide": if (inputValue != 0f) SetCurrentValue(currentValue / inputValue); break;
+                case "DivideInvoke": if (inputValue != 0f) SetCurrentValue(currentValue / inputValue); InvokeOutput(input); break;
+                case "Subtract": SetCurrentValue(currentValue - inputValue); break;
+                case "SubtractInvoke": SetCurrentValue(currentValue - inputValue); InvokeOutput(input); break;
+                case "SetValue": SetCurrentValue(inputValue); break;
+                case "SetValueInvoke": SetCurrentValue(inputValue); InvokeOutput(input); break;
+
+                case "SetMin": clampMin = inputValue; SetCurrentValue(currentValue); break;
+                case "SetMinInvoke": clampMin = inputValue; SetCurrentValue(currentValue); InvokeOutput(input); break;
+                case "SetMax": clampMax = inputValue; SetCurrentValue(currentValue); break;
+                case "SetMaxInvoke": clampMax = inputValue; SetCurrentValue(currentValue); InvokeOutput(input); break;
+
+                case "Freeze": frozen = true; break;
+                case "Unfreeze": frozen = false; SetCurrentValue(currentValue); break;
+                case "Invoke": SetCurrentValue(currentValue); InvokeOutput(input); break; // force a set for current value unity editor inspector changes.
             }
         }
 
